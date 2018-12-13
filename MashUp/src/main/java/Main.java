380cf70c09dc6acf218ef662cee5d6a75fa304dd
Romina Ferrario <saemyferrario@outlook.com>
2018-12-13 15:50:32 +0100

    Projektantrag

--- MashUp/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import dbAufruf.DbConnection;
-
-public class Main {
-    public static void main(String[] args){
-        DbConnection conn = new DbConnection();
-        conn.connection("account");
-    }
-}
